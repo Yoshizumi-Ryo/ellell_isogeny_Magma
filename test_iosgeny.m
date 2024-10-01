cd011afb317f7f3a633e93f64b1c35a72131aeff
Yoshizumi-Ryo <138884581+Yoshizumi-Ryo@users.noreply.github.com>
2024-10-02 06:04:31 +0900

    Add files via upload

--- test_iosgeny.m
+++ test_iosgeny.m
@@ -1,6 +1,7 @@
 
 
-p:=1536977231315969305425444639443786099;
+p:=4696592703174116824165605645274228079;
+
 l:=5;
 K:=GF(p^2);
 
@@ -17,4 +18,32 @@ lmd_data_pow12:=Product_power_lambda([tc_e1,tc_e2,tc_e12],l,lmd_data_1212);
 img_tc_x:=EvalOne(tc_0,[tc_e1,tc_e2,tc_e12],[tc_x,tc_xpe1,tc_xpe2],lmd_data_pow12,l,K);
 
 
-Time_for_isogeny(10);
+Time_for_isogeny(5);
+
+
+
+//================================================
+
+
+for t in [2^56..2^100] do
+    p:=-1+(2^2)*3*5*7*11*13*17*19*23*29*31*37*41*43*47*53*t;
+    if IsPrime(p) then
+        if Log(2,p) ge 120 then
+            Log(2,p);
+            print(p);
+            break;
+        end if;
+    end if;
+end for;
+
+
+
+for l in [2..55] do
+    if IsPrime(l) then
+        IsDivisibleBy(p+1,l);
+    end if;
+end for;
+
+
+
+
